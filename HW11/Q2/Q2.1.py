4096bbafa5a39577892d98f12111d3e12b9dc7ed
HosseinTahami <hosseintahami2000@gmail.com>
2023-06-19 08:38:58 +0330

    HW11,Q2,Part 1,Complete

--- HW11/Q2/Q2.1.py
+++ HW11/Q2/Q2.1.py
@@ -5,3 +5,7 @@ def calculate_mark_average(marks) :
     average = total / len(marks)
     return average
 
+if __name__ == "__main__":
+    marks = sys.argv[1:]
+    marks = [float(mark) for mark in marks]
+    print("Average: ", calculate_mark_average(marks))
