f61e9ceab3be03eacdc9e324963b6b2e90e659dd
HosseinTahami <hosseintahami2000@gmail.com>
2023-06-21 14:48:54 +0330

    add a comment

--- HW11/Q2/Q2.1.py
+++ HW11/Q2/Q2.1.py
@@ -9,3 +9,5 @@ if __name__ == "__main__":
     marks = sys.argv[1:]
     marks = [float(mark) for mark in marks]
     print("Average: ", calculate_mark_average(marks))
+
+# python3 Q2.1.py 20 0 4 55 53 2
