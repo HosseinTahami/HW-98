732ea524240b1a6228255929d71e62e3ff39c3a8
HosseinTahami <hosseintahami2000@gmail.com>
2023-06-24 16:31:06 +0330

    get_city_weather function Done !

--- HW12/weather_project/weather_server.py
+++ HW12/weather_project/weather_server.py
@@ -1,3 +1,9 @@
+import requests
+import json
+
+api_key = "d1b85a0024a2a5a7e6489e6b761b3ef8"
+base_url = "https://api.openweathermap.org/data/2.5/weather"
+
 def get_city_weather(city_name: str) -> dict:
     """
     Retrieve weather data from an external API for a given city.
@@ -6,12 +12,20 @@ def get_city_weather(city_name: str) -> dict:
     - city_name (str): The name of the city to retrieve weather data for.
 
     Returns:
-    - dict: A dictionary containing weather information for the city, including temperature, feels like temperature, and last updated time.
+    - dict: A dictionary containing weather information for the city, including:
+            -temperature
+            -feels like temperature
+            -last updated time
     """
-    pass
+    completed_url = base_url + "?q=" + city_name + "&appid=" + api_key
+    API_response = requests.get(completed_url)
+    api = API_response.json()
+    return api
 
 def start_server() -> None:
     """
     Start the weather server.
     """
     pass
+
+city_name = input("Enter a city name: ")
