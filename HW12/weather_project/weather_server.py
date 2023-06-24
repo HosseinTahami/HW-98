44567eedc528dbfab3ec953e95f68541da57f858
HosseinTahami <hosseintahami2000@gmail.com>
2023-06-24 18:26:31 +0330

    get_city_weather function Complete !

--- HW12/weather_project/weather_server.py
+++ HW12/weather_project/weather_server.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from datetime import datetime
 
 api_key = "d1b85a0024a2a5a7e6489e6b761b3ef8"
 base_url = "https://api.openweathermap.org/data/2.5/weather"
@@ -20,7 +21,12 @@ def get_city_weather(city_name: str) -> dict:
     completed_url = base_url + "?q=" + city_name + "&appid=" + api_key
     API_response = requests.get(completed_url)
     api = API_response.json()
-    return api
+    api_dict = {
+        'temperature': "{:.2f}".format(api["main"]["temp"] - 273.15),
+        'feels_like' : "{:.2f}".format(api["main"]["feels_like"] - 273.15),
+        'last_updated' : datetime.fromtimestamp(api['dt']).strftime('%Y-%m-%d %H:%M:%S')
+    }
+    return api_dict
 
 def start_server() -> None:
     """
