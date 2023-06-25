ed55137f9f94456a0039ec2c414cd83c098042b3
HosseinTahami <hosseintahami2000@gmail.com>
2023-06-25 16:36:11 +0330

    start_client ready

--- HW12_V2/weather_client.py
+++ HW12_V2/weather_client.py
@@ -1,5 +1,20 @@
 import requests
 import json
+import weather_server
 
 def start_client():
-    pass
+    while True:
+        response = requests.get(f"http://{weather_server.HOST}:{weather_server.PORT}/{city}")
+        data = response.json()
+        if 'message' in data:
+            print('Error: ',{data['message']})
+        else:
+            print(f'Temperature: {data["main"]["temp"]}°C')
+            print(f'Feels like: {data["main"]["feels_like"]}°C')
+            print(f'Last updated: {data["dt"]}')
+            
+
+
+city = input("Enter city name: ")
+start_client(city)
+    
