fd291e7c89675376a45123f9d5b377e029ce4b13
HosseinTahami <hosseintahami2000@gmail.com>
2023-06-25 15:41:56 +0330

    change some variable name

--- HW12_V2/weather_server.py
+++ HW12_V2/weather_server.py
@@ -1,5 +1,6 @@
 from http.server import BaseHTTPRequestHandler , HTTPServer
 import requests
+import json
 from datetime import datetime
 
 api_key = "d1b85a0024a2a5a7e6489e6b761b3ef8"
@@ -10,28 +11,30 @@ PORT = 9999
 def get_city_weather(city_name: str) -> dict:
     
     completed_url = base_url + "?q=" + city_name + "&appid=" + api_key
-    API_response = requests.get(completed_url)
-    api = API_response.json()
-    print(API_response.content)
+    response = requests.get(completed_url)
+    api = response.json()
+    #print(API_response.content)
     
-    if API_response.status_code == 200 :    
-        api_dict = {
+    if response.status_code == 200 :    
+        weather_dict = {
             'temperature': "{:.2f}".format(api["main"]["temp"] - 273.15),
             'feels_like' : "{:.2f}".format(api["main"]["feels_like"] - 273.15),
             'last_updated' : datetime.fromtimestamp(api['dt']).strftime('%Y-%m-%d %H:%M:%S')
         }
-        return api_dict
+        return weather_dict
 
-    if API_response.status_code == 404 :
+    if response.status_code == 404 :
         return "city not found"    
-
+    else :
+        return "Something went wrong!"
 class WeatherRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
+        city = self.path[1:]
         weather_dict = get_city_weather(city)
         self.send_response(200)
         self.send_header("Content-Type", "application/json")
         self.end_headers()
-        self.wfile.write(weather_dict)
+        self.wfile.write(json.dumps(weather_dict).encode())
         
    
 def start_server() -> None :
@@ -40,4 +43,5 @@ def start_server() -> None :
     server.server_forever()
     server.server_close()
     
-        
+
+start_server()
