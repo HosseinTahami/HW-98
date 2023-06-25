1bccea47405e9d9eb03c7ad6d7579455af42ac32
HosseinTahami <hosseintahami2000@gmail.com>
2023-06-25 23:41:18 +0330

    cleaning the sql

--- HW11/Q6/3.sql
+++ HW11/Q6/3.sql
@@ -1,7 +1,7 @@
 SELECT country.country, COUNT(*) AS customer_count
-FROM customer 
-JOIN address ON customer.address_id = address.address_id
-JOIN city ON address.city_id = city.city_id
-JOIN country ON city.country_id = country.country_id
+FROM customer
+    JOIN address ON customer.address_id = address.address_id
+    JOIN city ON address.city_id = city.city_id
+    JOIN country ON city.country_id = country.country_id
 GROUP BY country.country
 ORDER BY country DESC;
