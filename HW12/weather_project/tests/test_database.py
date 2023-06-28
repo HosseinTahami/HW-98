75f717894a56c7a01d297ede92c58fd0c163bd9c
HosseinTahami <hosseintahami2000@gmail.com>
2023-06-28 19:56:37 +0330

    test_save_request_data , test_save_response_date

--- HW12/weather_project/tests/test_database.py
+++ HW12/weather_project/tests/test_database.py
@@ -1,8 +1,36 @@
 from database import WeatherDatabase
 import pytest
 
-class TestWeatherDatabase:
+
+
+
+@pytest.fixture
+def test_db():
+    db = WeatherDatabase(database = 'test',
+                         user = 'postgres',
+                         password = 'iran1379',
+                         host = 'localhost',
+                         port = '5432')
+    db.cur.execute("delete from requests")
+    db.cur.execute("delete from responses")
+    db.conn.commit()
+    return db  
+
+def test_save_request_data(test):
+    test.save_request_data('Madrid', '2020-09-11 11:42:00')
+    test.cur.execute("select count(*) from requests")
+    assert test.cur.fetchone()[0] == 1
     
-    @pytest.fixture
-    def setup(self):
-        pass   
+def test_save_response_data(test):
+    testing_data = {
+        
+        'temperature': 23.56,
+        'feels_like' : 78.99,
+        'last_updated' : '2023-01-01 00:00:00'
+    }
+    
+    test.save_response_data("Madrid", testing_data)
+    
+    
+    
+    test.save_request_data()
