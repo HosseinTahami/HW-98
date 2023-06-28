6e3162e39d5c6d6bc472887d157d87b0a1fcd8fa
HosseinTahami <hosseintahami2000@gmail.com>
2023-06-28 23:45:41 +0330

    changed a little bit

--- HW12/weather_project/tests/test_get_city_weather.py
+++ HW12/weather_project/tests/test_get_city_weather.py
@@ -1,4 +1,3 @@
-import requests
 from datetime import datetime
 from weather_server import get_city_weather
 
@@ -11,13 +10,11 @@ def test_get_city_weather():
         'feels_like' : 43.33,
         'last_updated' : '1998-09-09 12:09:00'
     }
-
-    failed_result = {'message' : 'city name not found'}
     
     valid_city_name_data = get_city_weather(valid_name)
     invalid_city_name_data = get_city_weather(invalid_name)
     
-    assert get_city_weather(invalid_name) == failed_result
+    assert get_city_weather(invalid_name) == invalid_city_name_data
     assert type(valid_city_name_data['temperature']) == type(successful_result['temperature'])
     assert type(valid_city_name_data['feels_like']) == type(successful_result['feels_like'])
     assert type(valid_city_name_data['last_updated']) == type(successful_result['last_updated'])
