effe46506bb5861d22717e55d68bc558b6666826
HosseinTahami <hosseintahami2000@gmail.com>
2023-06-28 11:42:56 +0330

    cleaning the database

--- HW12_V2/database.py
+++ HW12_V2/database.py
@@ -35,34 +35,23 @@ class WeatherDatabase:
             last_updated = response_data['last_updated']
             success_code = 1
         
-        self.cur.execute("INSERT INTO responses (city_name, success_code, temperature, feels_like, last_updated) VALUES ( %s, %s, %s, %s, %s)",
-                        (city_name, success_code, temperature, feels_like, last_updated)
-                        )
+        self.cur.execute("""INSERT INTO responses (city_name, success_code, temperature, feels_like, last_updated) 
+                            VALUES ( %s, %s, %s, %s, %s)""", (city_name, success_code, temperature, feels_like, last_updated))
         self.conn.commit()
         
     def get_request_count(self) -> int:
-
-        self.cur.execute("SELECT COUNT(*) AS row_count FROM responses")
+        self.cur.execute("""SELECT COUNT(*) AS row_count FROM responses""")
         return self.cur.fetchone()[0]
         
     def get_successful_request_count(self) -> int:
-        """
-        Get the total number of successful requests made to the server.
-
-        Returns:
-        - int: The total number of successful requests made to the server.
-        """
-        self.cur.execute("SELECT COUNT(*) AS row_count FROM responses WHERE success_code = '1'")
+        self.cur.execute("""SELECT COUNT(*) AS row_count
+                            FROM responses 
+                            WHERE success_code = '1'""")
         return self.cur.fetchone()[0]
     
     
     def get_city_request_count(self) -> List[Tuple[str, int]]:
-        """
-        Get a count of requests made for each city.
 
-        Returns:
-        - List[Tuple[str, int]]: A list of tuples containing the name of the city and the number of requests made for that city.
-        """
         self.cur.execute("SELECT city_name, COUNT(*) AS request_count FROM requests GROUP BY city_name")
         request_list = []
         for city in self.cur.fetchall():
