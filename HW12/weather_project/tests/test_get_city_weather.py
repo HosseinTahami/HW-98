27820ef2b4e211833628cdff34b952deb7b630ff
HosseinTahami <hosseintahami2000@gmail.com>
2023-06-29 07:36:46 +0330

    test_get_city completed

--- HW12/weather_project/tests/test_get_city_weather.py
+++ HW12/weather_project/tests/test_get_city_weather.py
@@ -8,6 +8,6 @@ def test_get_city_weather():
     invalid_city_name_data = get_city_weather(invalid_name)
     
     assert get_city_weather(invalid_name) == invalid_city_name_data
-    assert ['temperature'] in valid_city_name_data
-    assert ['feels_like'] in valid_city_name_data
-    assert['last_updated'] in valid_city_name_data
+    assert 'temperature' in valid_city_name_data.keys()
+    assert 'feels_like' in valid_city_name_data.keys()
+    assert 'last_updated' in valid_city_name_data.keys()
